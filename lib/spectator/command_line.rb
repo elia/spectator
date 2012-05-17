f40a906b4a3932337d583b921406ca81a9da7dfe
Elia Schito <elia@schito.me>
2012-05-17 11:06:27 +0200

    Refix stty output

--- lib/spectator/command_line.rb
+++ lib/spectator/command_line.rb
@@ -1,7 +1,7 @@
 module Spectator
   module CommandLine
     def terminal_columns
-      cols = `stty -a 2&>1`.scan(/ (\d+) columns/).flatten.first
+      cols = `stty -a 2>&1`.scan(/ (\d+) columns/).flatten.first
       $?.success? ? cols.to_i : 0
     end
 
