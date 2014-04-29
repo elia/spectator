dd72eda0a0448cfecaf7742b31881cfbf221b29d
Elia Schito <elia@schito.me>
2014-04-29 15:36:57 +0200

    Extract core into a Runner class

--- lib/spectator.rb
+++ lib/spectator.rb
@@ -23,60 +23,61 @@ module Spectator
     end
   end
 
-  def path_watcher
-    @path_watcher ||= PathWatcher.new(config)
-  end
-
-  def ui
-    @ui ||= UI.new(config)
-  end
-
-  def spec_runner
-    @spec_runner ||= SpecRunner.new(config)
-  end
-
-  def success_notifier
-    @success_notifier ||= SuccessNotifier.new(config)
-  end
-
-  def run
-    $spectator_debug = config.debug
-
-    path_watcher.on_change { ui << :run_specs }
-    path_watcher.watch_paths!
-
-    ui.on(:run_specs) do
-      next unless ui.can_run_specs?
-
-      ui.status = :running_specs
-      files = path_watcher.pop_files
-      specs = SpecsMatcher.new(config).specs_for(files)
-      result = ui.run spec_runner.command(specs)
-      success_notifier.notify(result)
-      ui.status = nil if ui.status == :running_specs
+  class Runner
+    def initialize(config)
+      @config            = config
+      @path_watcher      = PathWatcher.new(config)
+      @ui                = UI.new(config)
+      @spec_runner       = SpecRunner.new(config)
+      @success_notifier  = SuccessNotifier.new(config)
+      @specs_matcher     = SpecsMatcher.new(config)
     end
 
-    ui.on(:interrupt) do
-      puts ' (Interrupted with CTRL+C)'.red
-      case ui.status
-      when :wait_for_input then ui.exit
-      when :running_specs  then ui.noop
-      when :exiting        then Kernel.abort
-      else Thread.new { ui.ask_what_to_do }
+    attr_reader :config, :path_watcher, :ui, :spec_runner, :success_notifier, :specs_matcher
+
+    def run
+      $spectator_debug = config.debug
+
+      path_watcher.on_change { ui << :run_specs }
+      path_watcher.watch_paths!
+
+      ui.on(:run_specs) do
+        next unless ui.can_run_specs?
+
+        ui.status = :running_specs
+        files = path_watcher.pop_files
+        specs = specs_matcher.specs_for(files)
+        result = ui.run spec_runner.command(specs)
+        success_notifier.notify(result)
+        ui.status = nil if ui.status == :running_specs
       end
+
+      ui.on(:interrupt) do
+        puts ' (Interrupted with CTRL+C)'.red
+        case ui.status
+        when :wait_for_input then ui.exit
+        when :running_specs  then ui.noop
+        when :exiting        then Kernel.abort
+        else Thread.new { ui.ask_what_to_do }
+        end
+      end
+
+      trap('INT') { ui << :interrupt }
+
+      ui.on(:run_all) do
+        next unless ui.can_run_specs?
+
+        ui.status = :running_specs
+        result = ui.run(spec_runner.command)
+        success_notifier.notify(result)
+        ui.status = nil if ui.status == :running_specs
+      end
+
+      ui.start
     end
+  end
 
-    trap('INT') { ui << :interrupt }
-
-    ui.on(:run_all) do
-      next unless ui.can_run_specs?
-
-      ui.status = :running_specs
-      result = ui.run(spec_runner.command)
-      success_notifier.notify(result)
-      ui.status = nil if ui.status == :running_specs
-    end
-
-    ui.start
+  def run(*args, &block)
+    Runner.new(*args, &block)
   end
 end
